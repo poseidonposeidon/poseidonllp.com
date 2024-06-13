8801afdec7dd30532cc51a0c580cf690f3f740c2
poseidonposeidon <poseidon@poseidonllp.com>
2024-06-13 12:17:36 +0800

    拜託

--- ftp_upload.py
+++ ftp_upload.py
@@ -1,3 +1,4 @@
+# ftp_upload.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ftplib import FTP
@@ -6,10 +7,10 @@ from werkzeug.utils import secure_filename
 import urllib.parse
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 设置为500MB
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 設置為500MB
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# 设置FTP服务器信息
+# 設置FTP伺服器信息
 FTP_HOST = '114.32.65.180'
 FTP_USER = 'Henry'
 FTP_PASS = '123456'
@@ -18,10 +19,10 @@ FTP_PASS = '123456'
 def upload_to_ftp():
     try:
         if 'file' not in request.files:
-            return jsonify({"error": "没有上传文件"}), 400
+            return jsonify({"error": "沒有上傳文件"}), 400
         file = request.files['file']
         if file.filename == '':
-            return jsonify({"error": "文件名为空"}), 400
+            return jsonify({"error": "文件名為空"}), 400
         if file:
             filename = secure_filename(file.filename)
             file.save(filename)
@@ -36,13 +37,13 @@ def upload_to_ftp():
                     ftp.storbinary(f'STOR %s' % urllib.parse.quote(filename), f)
                 ftp.quit()
             except Exception as e:
-                return jsonify({"error": f"FTP上传失败: {e}"}), 500
+                return jsonify({"error": f"FTP上傳失敗: {e}"}), 500
             finally:
                 ftp.close()
-                os.remove(filename)
+                os.remove(filename)  # 上傳完成後刪除本地文件
 
-            return jsonify({"message": f"文件 {filename} 已成功上传到FTP服务器"}), 200
-        return jsonify({"error": "无效的文件"}), 400
+            return jsonify({"message": f"文件 {filename} 已成功上傳到FTP伺服器"}), 200
+        return jsonify({"error": "無效的文件"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -50,18 +51,17 @@ def upload_to_ftp():
 def list_files():
     try:
         ftp = FTP()
-        ftp.set_debuglevel(0)
+        ftp.set_debuglevel(0)  # 禁用詳細的調試日誌
         ftp.connect(FTP_HOST)
         ftp.login(FTP_USER, FTP_PASS)
-        ftp.set_pasv(True)
-
+        ftp.set_pasv(True)  # 啟用被動模式
         files = ftp.nlst()
-        filtered_files = [file for file in files if file != 'ssl']
         ftp.quit()
-        files_decoded = [urllib.parse.unquote(f) for f in filtered_files]
+        # 確保文件名以UTF-8格式進行解碼
+        files_decoded = [urllib.parse.unquote(f) for f in files]
         return jsonify({"files": files_decoded})
     except Exception as e:
-        print(f"Error in list_files: {e}")
+        print(f"Error in list_files: {e}")  # 添加日誌
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
