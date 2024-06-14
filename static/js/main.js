47e6d4dcd7a511a5a43d84c9bd07d5e95af7345a
poseidonposeidon <poseidon@poseidonllp.com>
2024-06-14 12:42:54 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -916,7 +916,7 @@ function fetchFileList() {
     console.log("Fetching file list from server...");
     fetch('https://api.poseidonllp.com/list_files', {
         method: 'GET',
-        mode: 'no-cors',  // 設置 CORS 模式
+        mode: 'cors',  // 設置 CORS 模式
         credentials: 'include'  // 包含憑據（如有需要）
     })
         .then(response => {
