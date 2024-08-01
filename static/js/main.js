f59ef2c49c2332233996562c25e9f665c537c856
poseidonposeidon <poseidon@poseidonllp.com>
2024-08-01 13:12:17 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -907,9 +907,9 @@ function displayIncomeStatement(data, container, chartId, operatingChartId, peri
 
     container.innerHTML = `
         <div class="scroll-container-x" id="${chartId}ScrollContainer">
-            <div class="drag-overlay"></div>
+            
             <div id="${chartId}Container">
-                ${tableHtml}
+                <div class="drag-overlay">${tableHtml}</div>
             </div>
         </div>
         <div id="operatingChartContainer" style="margin-top: 20px;">
