a85f175b7d16bf11a2567cdb3084b649ce6c8d7b
poseidonposeidon <poseidon@poseidonllp.com>
2024-09-05 21:38:42 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -5027,17 +5027,17 @@ function displayInsiderTrades(data, container) {
         rows.link.push(item.link ? `<a class="styled-link" href="${item.link}" target="_blank">View Form</a>` : 'N/A');
     });
 
-    // 構建 HTML 表格
+    // 構建 HTML 表格，使用 white-space: nowrap; 防止內容換行
     let tableHtml = `
     <div style="display: flex; overflow-x: auto;">
         <div style="flex-shrink: 0; background: #1e1e1e; z-index: 1; border-right: 1px solid #000;">
-            <table border="1" style="border-collapse: collapse;">
-                ${Object.keys(rows).map(key => `<tr><th>${rows[key][0]}</th></tr>`).join('')}
+            <table border="1" style="border-collapse: collapse; white-space: nowrap;">
+                ${Object.keys(rows).map(key => `<tr><th style="white-space: nowrap;">${rows[key][0]}</th></tr>`).join('')}
             </table>
         </div>
         <div class="scroll-right" style="overflow-x: auto;">
-            <table border="1" style="width: 100%; border-collapse: collapse;">
-                ${Object.keys(rows).map(key => `<tr>${rows[key].slice(1).map(value => `<td>${value}</td>`).join('')}</tr>`).join('')}
+            <table border="1" style="width: 100%; border-collapse: collapse; white-space: nowrap;">
+                ${Object.keys(rows).map(key => `<tr>${rows[key].slice(1).map(value => `<td style="white-space: nowrap;">${value}</td>`).join('')}</tr>`).join('')}
             </table>
         </div>
     </div>
