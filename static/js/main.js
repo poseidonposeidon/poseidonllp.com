e85fd0dd5cb80ead504b50478a5eececd83c3050
poseidonposeidon <poseidon@poseidonllp.com>
2024-09-12 14:32:04 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1492,9 +1492,16 @@ function addEnterKeyListener(inputId, buttonSelector) {
 
             // 隱藏對應的建議框
             clearSuggestions();
+
+            // 延遲 0.3 秒再次清除建議框，確保不會再次顯示
+            setTimeout(() => {
+                clearSuggestions();
+            }, 300); // 300 毫秒
         }
     });
 }
+
+
 addEnterKeyListener("stockSymbol", "#usStockButton");
 addEnterKeyListener("jpStockSymbol", "#jpStockButton");
 addEnterKeyListener("twStockSymbol", "#twStockButton");
