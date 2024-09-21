182165b674a720ff0da7f21b548f52c96c326e99
poseidonposeidon <poseidon@poseidonllp.com>
2024-09-21 10:39:10 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1046,7 +1046,7 @@ function sendMessage() {
         // 發送請求到 Flask 後端
         fetch(`${baseUrl}/chat_openai`, {
             method: 'POST',
-            // mode: 'no-cors',
+            mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
             },
