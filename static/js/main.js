86e2d521d5e9a1a9867086641b1ce4963608bc72
poseidonposeidon <poseidon@poseidonllp.com>
2025-02-23 21:52:04 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -4032,6 +4032,7 @@ function drawChart(labels, dataSets, type) {
                     maxBarThickness: 50,
                     barPercentage: 0.9,
                     categoryPercentage: 1.0,
+                    stacked: false
                 }
             }
         }
